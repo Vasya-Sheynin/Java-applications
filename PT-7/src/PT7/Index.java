3b8b4b5dd44698872a430d14eb29a05fa334cb7b
Vasya-Sheynin <144619835+Vasya-Sheynin@users.noreply.github.com>
2024-06-26 14:26:53 +0300

    Add files via upload

---
+++ PT-7/src/PT7/Index.java
@@ -0,0 +1,135 @@
+package PT7;
+
+import java.io.*;
+import java.util.*;
+import java.util.zip.*;
+
+class KeyComp implements Comparator<String> {
+    public int compare(String o1, String o2) {
+        // right order:
+        return o1.compareTo(o2);
+    }
+}
+
+class KeyCompReverse implements Comparator<String> {
+    public int compare(String o1, String o2) {
+        // reverse order:
+        return o2.compareTo(o1);
+    }
+}
+
+interface IndexBase {
+    String[] getKeys(Comparator<String> comp);
+
+    void put(String key, long value);
+
+    boolean contains(String key);
+
+    Long[] get(String key);
+}
+
+class IndexOne2N implements Serializable, IndexBase {
+    // Not unique keys
+    // class release version:
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final TreeMap<String, Vector<Long>> map;
+
+    public IndexOne2N() {
+        map = new TreeMap<String, Vector<Long>>();
+    }
+
+    public String[] getKeys(Comparator<String> comp) {
+        String[] result = map.keySet().toArray(new String[0]);
+        Arrays.sort(result, comp);
+        return result;
+    }
+
+    public void put(String key, long value) {
+        Vector<Long> arr = map.get(key);
+        if (arr == null) {
+            arr = new Vector<Long>();
+        }
+        arr.add(value);
+        map.put(key, arr);
+    }
+
+    public boolean contains(String key) {
+        return map.containsKey(key);
+    }
+
+    public Long[] get(String key) {
+        return map.get(key).toArray(new Long[0]);
+    }
+}
+
+public class Index implements Serializable, Closeable {
+    // class release version:
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    IndexOne2N fullNames;
+    IndexOne2N telNumbers;
+    IndexOne2N dates;
+
+    public void put(ConversData conversData, long value) {
+        fullNames.put(conversData.personFullName, value);
+        telNumbers.put(conversData.telNumber, value);
+        dates.put(conversData.date, value);
+    }
+
+    public Index() {
+        fullNames = new IndexOne2N();
+        telNumbers = new IndexOne2N();
+        dates = new IndexOne2N();
+    }
+
+    public static Index load(String name) throws IOException,
+            ClassNotFoundException {
+        Index obj = null;
+        try {
+            FileInputStream file = new FileInputStream(name);
+            try (ZipInputStream zis = new ZipInputStream(file)) {
+                ZipEntry zen = zis.getNextEntry();
+                assert zen != null;
+                if (!zen.getName().equals(Buffer.zipEntryName)) {
+                    throw new IOException("Invalid block format");
+                }
+                try (ObjectInputStream ois = new ObjectInputStream(zis)) {
+                    obj = (Index) ois.readObject();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            obj = new Index();
+        }
+        if (obj != null) {
+            obj.save(name);
+        }
+        return obj;
+    }
+
+    private transient String filename = null;
+
+    public void save(String name) {
+        filename = name;
+    }
+
+    public void saveAs(String name) throws IOException {
+        FileOutputStream file = new FileOutputStream(name);
+        try (ZipOutputStream zos = new ZipOutputStream(file)) {
+            zos.putNextEntry(new ZipEntry(Buffer.zipEntryName));
+            zos.setLevel(ZipOutputStream.DEFLATED);
+            try (ObjectOutputStream oos = new ObjectOutputStream(zos)) {
+                oos.writeObject(this);
+                oos.flush();
+                zos.closeEntry();
+                zos.flush();
+            }
+        }
+    }
+
+    public void close() throws IOException {
+        saveAs(filename);
+    }
+}
